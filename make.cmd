7477b30d3f8417ea4b2c77ea0152f23f0ae53c1c
Tom Flídr <tomFlidr@users.noreply.github.com>
2017-01-18 01:54:44 +0100

    Update make.cmd

--- make.cmd
+++ make.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 :: standard way to create phar archive (much slower)
-::php phar.php
+::php make-phar.php
 
 :: advanced way to create single php file with base64 encoded static files (much faster)
 php make-php.php
